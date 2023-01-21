4c6d85d5dec6bdaa9873673ef1d40a4d211fe805
Anatolii Volokhov <avolokhov@uma.tech>
2023-01-22 05:50:12 +0700

    update git links

--- scripts/uploadRelease.js
+++ scripts/uploadRelease.js
@@ -26,7 +26,7 @@ const main = async () => {
 
   try {
     const { data: releasesList } = await axios.default.get(
-      `https://api.github.com/repos/gorilla-devs/GDLauncher/releases`,
+      `https://api.github.com/repos/Proxwian/OxLAUNCHER/releases`,
       {
         headers: {
           Authorization: `token ${process.env.GH_ACCESS_TOKEN_RELEASES}`
@@ -57,10 +57,10 @@ const main = async () => {
             const prSplit = e?.advanced?.pr && e?.advanced?.pr.split('/');
             const advanced =
               e?.advanced?.cm &&
-              ` ([${e?.advanced?.cm}](https://github.com/gorilla-devs/GDLauncher/commit/${e?.advanced?.cm})` +
+              ` ([${e?.advanced?.cm}](https://github.com/Proxwian/OxLAUNCHER/commit/${e?.advanced?.cm})` +
                 `${
                   prSplit
-                    ? ` | [#${e?.advanced.pr}](https://github.com/gorilla-devs/GDLauncher/pull/${prSplit[0]}` +
+                    ? ` | [#${e?.advanced.pr}](https://github.com/Proxwian/OxLAUNCHER/pull/${prSplit[0]}` +
                       `${prSplit?.[1] ? `/commits/${prSplit[1]}` : ''})`
                     : ''
                 })`;
@@ -73,7 +73,7 @@ const main = async () => {
     };
 
     const { data: newRelease } = await axios.default.post(
-      'https://api.github.com/repos/gorilla-devs/GDLauncher/releases',
+      'https://api.github.com/repos/Proxwian/OxLAUNCHER/releases',
       {
         tag_name: `v${version}`,
         name: `v${version}`,
