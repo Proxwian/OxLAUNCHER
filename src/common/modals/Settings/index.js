f9aa0f8c5d6396b43de424b9b0de04f9df17d5f7
Anatolii Volokhov <avolokhov@uma.tech>
2023-05-23 13:17:23 +0700

    up version

--- src/common/modals/Settings/index.js
+++ src/common/modals/Settings/index.js
@@ -156,7 +156,7 @@ export default function Settings() {
                 font-size: 16px;
               `}
             >
-              OxLAUNCHER 1.2.7
+              OxLAUNCHER 1.2.8
             </span>
 			<p>by Proxwian</p>
             <div
