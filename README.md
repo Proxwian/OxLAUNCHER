1ab35e6fd9a98056affbab2e4a7ab6242da66d04
Anatolii Volokhov <avolokhov@uma.tech>
2023-08-06 02:04:45 +0700

    1.2.10

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # OxLAUNCHER
 
-Latest version: 1.2.10
+Latest version: 1.2.11
--- src/common/modals/Settings/index.js
+++ src/common/modals/Settings/index.js
@@ -156,7 +156,7 @@ export default function Settings() {
                 font-size: 16px;
               `}
             >
-              OxLAUNCHER 1.2.10
+              OxLAUNCHER 1.2.11
             </span>
 			<p>by Proxwian</p>
             <div
